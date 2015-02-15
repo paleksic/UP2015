de6ea198835e905c7a07ae1c7cc29f19c160baf2
paleksic <paleksic083@gmail,com>
2015-02-15 13:00:46 +0100

    5

--- up2015/src/main/java/Rekurzija/zad05.java
+++ up2015/src/main/java/Rekurzija/zad05.java
@@ -4,5 +4,19 @@ package Rekurzija;
  * Created by paleksic on 2/15/2015.
  * paleksic083@gmail.com
  */
-public class zad05 {
+public class zad05 implements rekurzija {
+    @Override
+    public double f_iter(int n) {
+        return 0;
+    }
+
+    @Override
+    public double f_iter(int i, int m) {
+        return 0;
+    }
+
+    @Override
+    public double f_rek(int i, int n) {
+        return 0;
+    }
 }
